0f66fec380198a13fff61f77fd66d044d68e9be3
Burak Mike <burakmahyk@gmail.com>
2022-09-01 23:44:51 +0300

    refactor: cleanup

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,6 +1,4 @@
 import * as trpc from '@trpc/server'
-import * as trpcNext from '@trpc/server/adapters/next'
-import { z } from 'zod'
 import superjson from 'superjson'
 import { LinkRouter } from './link'
 export const appRouter = trpc
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,4 @@
-import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
-import { z } from 'zod'
 import { appRouter } from '../../../backend/router'
 // export type definition of API
 export type AppRouter = typeof appRouter
